8639ceab69d24071ec8b334f4037637ef4714fd6
1224police@likelion.org <1224police@likelion.org>
2022-02-13 02:32:37 +0900

    [finish]Update chatroom and chatmessage

--- websocket-first/src/main/java/com/eunbee/websocketfirst/handler/WebSockChatHandler.java
+++ websocket-first/src/main/java/com/eunbee/websocketfirst/handler/WebSockChatHandler.java
@@ -2,21 +2,34 @@ package com.eunbee.websocketfirst.handler;
 
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.eunbee.websocketfirst.model.ChatMessage;
+import com.eunbee.websocketfirst.model.ChatRoom;
+import com.eunbee.websocketfirst.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.socket.TextMessage;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class WebSockChatHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
- @Override
- protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-     String payload = message.getPayload();
-     log.info("payload {}", payload);
-     TextMessage textMessage = new TextMessage("Welcome chatting sever~^^");
-     session.sendMessage(textMessage);
- }
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+// 삭제        TextMessage textMessage = new TextMessage("Welcome chatting sever~^^ ");
+// 삭제       session.sendMessage(textMessage);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
+    }
 }
